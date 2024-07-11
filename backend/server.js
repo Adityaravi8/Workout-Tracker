8eb9e3d696b53b5789edfc59abaf6a94289b080e
Aditya Ravi <94879854+Adityaravi8@users.noreply.github.com>
2024-07-11 19:42:47 -0400

    Setup Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("Successfully connected to db"));
 
 app.listen(process.env.PORT, () => {
   console.log("Connected to port: ", process.env.PORT);
